5bd9eaaa292b6b70034c3dc08801852f9ae9fc05
Simon Teo <teosimon78@gmail.com>
2020-03-22 22:42:20 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,33 +1,33 @@
 function basicTeenager(age) {
   if (age >= 13 && age <= 19)
   {
-    return "You are a teenager!"
+    return "You are a teenager!";
   }
 }
 
 function teenager(age) {
-  basicTeenager(age)
-  return "You are not a teenager"
+  basicTeenager(age);
+  return "You are not a teenager";
 }
 
 function ageChecker(age) {
-  basicTeenager(age)
+  basicTeenager(age);
   if (age >= 12)
   {
-    return 'You are a kid'
+    return 'You are a kid';
   }
   else {
     {
-      return 'You are a grownup'
+      return 'You are a grownup';
     }
   }
 }
 
 function ternaryTeenager(age) {
-  teenager(age)
+  teenager(age);
 }
 
 function switchAge(age) {
-  basicTeenager(age)
-  return 'You have an age'
+  basicTeenager(age);
+  return 'You have an age';
 }
